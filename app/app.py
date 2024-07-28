dfc7add5c9107e32aa0d760793c9aed6dd741fc1
VillageR88 <villager88pl@gmail.com>
2024-07-28 23:08:34 +0200

    dev1
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/app.py
+++ app/app.py
@@ -1,28 +1,26 @@
 from flask import Flask, render_template, send_from_directory
 
+
 def create_app():
     app = Flask(__name__)
 
-    @app.route('/')
+    @app.route("/")
     def index():
-        context = {
-            'siteTitle': "Coffeeroasters subscription site"
-            }
-          
-        return render_template('index.html', **context)
+        context = {"siteTitle": "Coffeeroasters subscription site"}
 
-    @app.route('/robots.txt')
+        return render_template("index.html", **context)
+
+    @app.route("/robots.txt")
     def robots_txt():
         try:
-            return send_from_directory(app.static_folder, 'robots.txt')
+            return send_from_directory(app.static_folder, "robots.txt")
         except FileNotFoundError:
             logging.error("robots.txt not found")
             return "robots.txt not found", 404
 
     return app
 
+
 if __name__ == "__main__":
     app = create_app()
     app.run(debug=True)
-    
-    
