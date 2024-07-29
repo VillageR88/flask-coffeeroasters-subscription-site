f899225865ce2157c7cfefd4dfb82d3e421b155f
VillageR88 <villager88pl@gmail.com>
2024-07-29 13:37:24 +0200

    initial 4th block
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/app.py
+++ app/app.py
@@ -45,10 +45,25 @@ def create_app():
                 "description": "We cover the cost and coffee is delivered fast. Peak freshness: guaranteed.",
             },
         ]
+        howItWorks = [
+            {
+                "title": "Pick your coffee",
+                "description": "Select from our evolving range of artisan coffees. Our beans are ethically sourced and we pay fair prices for them. There are new coffees in all profiles every month for you to try out.",
+            },
+            {
+                "title": "Choose the frequency",
+                "description": "Customize your order frequency, quantity, even your roast style and grind type. Pause, skip or cancel your subscription with no commitment through our online portal.",
+            },
+            {
+                "title": "Receive and enjoy!",
+                "description": "We ship your package within 48 hours, freshly roasted. Sit back and enjoy award-winning world-class coffees curated to provide a distinct tasting experience.",
+            },
+        ]
         context = {
             "siteTitle": "Coffeeroasters subscription site",
             "collection": collection,
             "whyChooseUs": whyChooseUs,
+            "howItWorks": howItWorks,
         }
 
         return render_template("home/index.html", **context)
